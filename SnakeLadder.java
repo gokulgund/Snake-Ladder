20a12ad39ca6db34a4553d06a024d5b9778431cd
gokul gund <gokulgund15@gmail.com>
2021-11-22 17:31:15 +0530

    Added : Roll The Dice Get Bet 1 to 6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,7 +5,13 @@ public class SnakeLadder {
 	public static void main(String[] args) {
 		int position=0;
 		System.out.println("Welcome into Snake & Ladder Program !!");
-//		System.out.println("Your Starting Position Is : " +position);
+		System.out.println("Your Starting Position Is : " +position);
+		Dice();
+	}
+	public static void Dice() {
+		int Dice = (int)Math.floor(Math.random()*10)%6+1;	
+		System.out.println("Your Dice Value is : " +Dice);
+	
 	}
 
 }
