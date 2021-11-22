1d6bb3d0ac4a4b28b4c636d037db98258a406947
gokul gund <gokulgund15@gmail.com>
2021-11-22 16:40:21 +0530

    Added : Starting Position

---
+++ SnakeLadder.java
@@ -0,0 +1,11 @@
+package snake_ladder;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		int position=0;
+		System.out.println("Welcome into Snake & Ladder Program !!");
+		System.out.println("Your Starting Position Is : " +position);
+	}
+
+}
